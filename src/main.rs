90d6625da00661a5b68a03bba6524e4496125c35
dimitrmo <dmouratidis153@gmail.com>
2023-01-29 12:39:42 +0200

    Testing fred lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use fred::{prelude::*};
+use fred::types::RespVersion::RESP3;
+
+#[tokio::main]
+async fn main() -> Result<(), RedisError> {
+  let config = RedisConfig {
+    server: ServerConfig::Sentinel {
+      service_name: "mymaster".into(),
+      hosts: vec![
+        ("localhost".into(), 26379),
+      ],
+    },
+    password: Some("".to_string()),
+    version: RESP3,
+    ..Default::default()
+  };
+
+  let policy = ReconnectPolicy::default();
+  let client = RedisClient::new(config, None, Some(policy));
+  let _ = client.connect();
+  let _ = client.wait_for_connect().await?;
+
+  // do stuff
+
+  let _ = client.quit().await?;
+  Ok(())
 }
