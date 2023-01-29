672ecfc2546d533f900834fdafddd08045370825
dimitrmo <dmouratidis153@gmail.com>
2023-01-29 13:05:44 +0200

    Dockerize

--- Makefile
+++ Makefile
@@ -11,3 +11,18 @@ build:
 prod:
 	cargo +nightly build --release
 	ls -lah target/release/hub
+
+.PHONY: docker-build
+docker-build:
+	docker build --squash --build-arg BINARY=hub -t hub:latest -f Dockerfile .
+
+.PHONY: docker-run
+docker-run:
+	docker run -it --rm --init \
+		-e RUST_BACKTRACE=1 \
+		-e RUST_LOG=1 \
+		-e BINARY=hub \
+		-e REDIS_PASSWORD=123 \
+		--network host \
+		--name hub \
+		hub:latest
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
 use fred::{prelude::*};
-use fred::types::RespVersion::RESP3;
 
 #[tokio::main]
 async fn main() -> Result<(), RedisError> {
+  let redis_password = env::var("REDIS_PASSWORD")
+      .expect("REDIS_PASSWORD is not set");
+
   let config = RedisConfig {
     server: ServerConfig::Sentinel {
       service_name: "mymaster".into(),
@@ -10,13 +13,15 @@ async fn main() -> Result<(), RedisError> {
         ("localhost".into(), 26379),
       ],
     },
-    password: Some("".to_string()),
-    version: RESP3,
+    password: Some(redis_password.to_string()),
     ..Default::default()
   };
 
+  println!("===== CLIENT config {:?}", config);
+
   let policy = ReconnectPolicy::default();
   let client = RedisClient::new(config, None, Some(policy));
+
   let _ = client.connect();
   let _ = client.wait_for_connect().await?;
 
