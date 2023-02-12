53b037c531d1107cd5d90254192e5590616fe97e
dimitrmo <dmouratidis153@gmail.com>
2023-02-12 20:34:41 +0200

    Set channels on start up

--- src/http/channels.rs
+++ src/http/channels.rs
@@ -1,7 +1,8 @@
 use crate::http::SharedState;
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use log::{debug, trace, warn};
-use rhiaqey_common::pubsub::{RPCMessage, RPCMessageType};
+use rhiaqey_common::pubsub::{RPCMessage, RPCMessageData};
+use rhiaqey_common::topics;
 use rhiaqey_sdk::channel::{AssignChannelsRequest, ChannelList, CreateChannelsRequest};
 use rustis::client::BatchPreparedCommand;
 use rustis::commands::{PubSubCommands, StreamCommands, StringCommands, XGroupCreateOptions};
@@ -13,22 +14,26 @@ pub async fn create_channels(
     Json(payload): Json<CreateChannelsRequest>,
     state: Arc<SharedState>,
 ) -> impl IntoResponse {
-    let key = format!("{}:channels", state.namespace);
+    let hub_channels_key = topics::hub_channels_key(state.namespace.clone());
     let content = serde_json::to_string(&payload).unwrap_or("{}".to_string());
+
     let mut pipeline = state.redis.lock().await.as_mut().unwrap().create_pipeline();
-    pipeline.set(key.clone(), content).forget();
+    pipeline.set(hub_channels_key.clone(), content).forget();
     for channel in &payload.channels.channels {
-        let topic = format!("{}:{}:streams:raw", state.namespace, channel.name);
         pipeline
             .xgroup_create(
-                topic,
+                topics::publishers_to_hub_stream_topic(
+                    state.namespace.clone(),
+                    channel.name.clone(),
+                ),
                 "hub",
                 "$",
                 XGroupCreateOptions::default().mk_stream(),
             )
             .forget();
     }
-    pipeline.get::<_, ()>(key).queue();
+
+    pipeline.get::<_, ()>(hub_channels_key).queue();
     let pipeline_result: RedisResult<Value> = pipeline.execute().await;
     trace!("pipeline result: {:?}", pipeline_result);
     (
@@ -43,9 +48,13 @@ pub async fn assign_channels(
     state: Arc<SharedState>,
 ) -> impl IntoResponse {
     let mut client = state.redis.lock().await.clone().unwrap();
-    let key = format!("{}:channels", state.namespace);
-    let result: String = client.get(key.clone()).await.unwrap();
+
+    // get channels
+
+    let channels_key = topics::hub_channels_key(state.namespace.clone());
+    let result: String = client.get(channels_key.clone()).await.unwrap();
     debug!("got channels {}", result);
+
     let channel_list: Result<ChannelList, _> = serde_json::from_str(result.as_str());
     if channel_list.is_err() {
         warn!(
@@ -55,6 +64,8 @@ pub async fn assign_channels(
         );
         return (StatusCode::BAD_REQUEST, Json(vec![]));
     }
+
+    // populate channels with details e.g. size
 
     let channels = channel_list
         .unwrap()
@@ -70,29 +81,34 @@ pub async fn assign_channels(
         .map(|x| x.clone())
         .collect::<Vec<_>>();
 
-    let key = format!("{}:{}:channels", state.namespace, payload.name);
-    let stream = format!(
-        "{}:{}:streams:pubsub",
-        state.namespace, payload.name /* publisher */
-    );
+    // update publisher's channels
 
-    debug!("publishing to {}", stream);
+    let publishers_key =
+        topics::publisher_channels_key(state.namespace.clone(), payload.name.clone());
+
+    debug!("saving channels for publisher {}", publishers_key);
 
     let content = serde_json::to_string(&ChannelList {
         channels: channels.clone(),
     })
     .unwrap();
-    client.set(key, content).await.unwrap();
+    client.set(publishers_key, content).await.unwrap();
 
-    debug!("streaming to {}", stream);
+    // stream to publisher
+
+    let stream_topic = topics::hub_to_publisher_pubsub_topic(state.namespace.clone(), payload.name);
+
+    debug!("streaming to topic {}", stream_topic);
 
     let content = serde_json::to_string(&RPCMessage {
-        data: RPCMessageType::AssignChannels(ChannelList {
+        data: RPCMessageData::AssignChannels(ChannelList {
             channels: channels.clone(),
         }),
     })
     .unwrap();
-    client.publish(stream, content).await.unwrap();
+    client.publish(stream_topic, content).await.unwrap();
+
+    // return response
 
     (StatusCode::OK, Json(channels))
 }
--- src/hub/mod.rs
+++ src/hub/mod.rs
@@ -2,16 +2,18 @@ use crate::http::start_http_server;
 use crate::http::state::SharedState;
 use log::debug;
 use rhiaqey_common::env::{parse_env, Env};
-use rhiaqey_common::redis;
+use rhiaqey_common::{redis, topics};
+use rhiaqey_sdk::channel::{Channel, ChannelList};
 use rustis::client::Client;
-use rustis::commands::{ConnectionCommands, PingOptions};
+use rustis::commands::{ConnectionCommands, PingOptions, StringCommands};
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::sync::{Mutex, RwLock};
 
 #[derive(Clone)]
 pub struct Hub {
     env: Arc<Env>,
-    redis: Arc<Mutex<Option<Client>>>,
+    channels: Arc<RwLock<Vec<Channel>>>,
+    pub(crate) redis: Arc<Mutex<Option<Client>>>,
 }
 
 impl Hub {
@@ -31,6 +33,36 @@ impl Hub {
         self.env.private_port
     }
 
+    pub async fn set_channels(&mut self, channels: Vec<Channel>) {
+        let mut locked_channels = self.channels.write().await;
+        *locked_channels = channels;
+    }
+
+    pub async fn get_channels(&self) -> Vec<Channel> {
+        let channels_key =
+            topics::publisher_channels_key(self.env.namespace.clone(), self.env.name.clone());
+
+        let result: String = self
+            .redis
+            .lock()
+            .await
+            .as_mut()
+            .unwrap()
+            .get(channels_key.clone())
+            .await
+            .unwrap();
+
+        let channel_list: ChannelList =
+            serde_json::from_str(result.as_str()).unwrap_or(ChannelList::default());
+
+        debug!(
+            "channels from {} retrieved {:?}",
+            channels_key, channel_list
+        );
+
+        channel_list.channels
+    }
+
     pub async fn setup(config: Env) -> Result<Hub, String> {
         let redis_connection = redis::connect(config.redis.clone()).await;
         let result: String = redis_connection
@@ -45,6 +77,7 @@ impl Hub {
 
         Ok(Hub {
             env: Arc::from(config),
+            channels: Arc::from(RwLock::new(vec![])),
             redis: Arc::new(Mutex::new(redis_connection)),
         })
     }
@@ -65,7 +98,7 @@ pub async fn run() {
     env_logger::init();
     let env = parse_env();
 
-    let hub = match Hub::setup(env).await {
+    let mut hub = match Hub::setup(env).await {
         Ok(exec) => exec,
         Err(error) => {
             panic!("failed to setup hub: {error}");
@@ -78,6 +111,9 @@ pub async fn run() {
         hub.get_name(),
         hub.is_debug()
     );
+
+    let channels = hub.get_channels().await;
+    hub.set_channels(channels).await;
 
     hub.start().await.unwrap()
 }
