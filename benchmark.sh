21be934e0dbce4ffba4cb77da775f9410a54a5b9
dimitrmo <dmouratidis153@gmail.com>
2024-04-05 09:16:09 +0300

    Add benchmark script

---
+++ benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start multiple instances of websocat in the background to simulate concurrent connections
+# shellcheck disable=SC2034
+for i in {1..4125}; do
+    websocat -n ws://localhost:3002/ws?channels=ticks &
+done
+
+# Wait for all background jobs to finish
+sleep 1000
+
+# Wait for a duration to allow connections to establish and messages to be exchanged
+# sleep 1000
+
+# Close all websocat instances
+pkill -f "websocat"
