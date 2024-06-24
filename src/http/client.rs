b5aae23fee035e35944d09b12cb9540c9262ce04
dimitrmo <dmouratidis153@gmail.com>
2024-06-24 16:14:17 +0300

    Return hub id

--- src/http/client.rs
+++ src/http/client.rs
@@ -1,10 +1,13 @@
+use crate::http::state::SharedState;
 use crate::hub::metrics::TOTAL_CLIENTS;
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use log::info;
 use serde_json::json;
+use std::sync::Arc;
 
-pub async fn get_users_handler() -> impl IntoResponse {
+pub async fn get_users_handler(State(state): State<Arc<SharedState>>) -> impl IntoResponse {
     let clients = TOTAL_CLIENTS.get();
     info!("[GET] get total connected users: {}", clients);
 
@@ -12,6 +15,7 @@ pub async fn get_users_handler() -> impl IntoResponse {
         StatusCode::OK,
         [(hyper::header::CONTENT_TYPE, "application/json")],
         json!({
+            "Hub": state.get_id(),
             "Clients": clients as i64
         })
         .to_string(),
